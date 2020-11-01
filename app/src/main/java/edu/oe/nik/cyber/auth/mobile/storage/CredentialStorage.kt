263ea24654491f098cfbaa739d1fdc39e7940a87
Gergely Béres <gergely.beres@adnovum.hu>
2020-11-01 18:55:39 +0100

    Work on Storage module

--- app/src/main/java/edu/oe/nik/cyber/auth/mobile/storage/CredentialStorage.kt
+++ app/src/main/java/edu/oe/nik/cyber/auth/mobile/storage/CredentialStorage.kt
@@ -4,8 +4,8 @@ import android.content.SharedPreferences
 import javax.inject.Inject
 
 class CredentialStorage @Inject constructor(
-    val insecureSharedPreferences: SharedPreferences,
-    val secureSharedPreferences: SharedPreferences
+    private val insecureSharedPreferences: SharedPreferences,
+    private val secureSharedPreferences: SharedPreferences
 ){
 
     companion object SharedPrefKeys {
@@ -18,11 +18,11 @@ class CredentialStorage @Inject constructor(
         get() = insecureSharedPreferences.getString(SharedPrefKeys.JWT_KEY, "")
         set(value) {
             with(insecureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.JWT_KEY, value)
+                putString(JWT_KEY, value)
                 apply()
             }
             with(secureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.JWT_KEY, value)
+                putString(JWT_KEY, value)
                 apply()
             }
         }
@@ -31,11 +31,11 @@ class CredentialStorage @Inject constructor(
         get() = insecureSharedPreferences.getString(SharedPrefKeys.TOTP_SECRET, "")
         set(value) {
             with(insecureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.TOTP_SECRET, value)
+                putString(TOTP_SECRET, value)
                 apply()
             }
             with(secureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.TOTP_SECRET, value)
+                putString(TOTP_SECRET, value)
                 apply()
             }
         }
@@ -44,11 +44,11 @@ class CredentialStorage @Inject constructor(
         get() = insecureSharedPreferences.getString(SharedPrefKeys.PREFERRED_AUTH_TYPE, "")
         set(value) {
             with(insecureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.PREFERRED_AUTH_TYPE, value)
+                putString(PREFERRED_AUTH_TYPE, value)
                 apply()
             }
             with(secureSharedPreferences.edit()) {
-                putString(SharedPrefKeys.PREFERRED_AUTH_TYPE, value)
+                putString(PREFERRED_AUTH_TYPE, value)
                 apply()
             }
         }
